de69d1b8ce3588c75764a3e04308334978d90407
Laura <laura.mastio@epitech.eu>
2023-12-07 11:50:56 +0100

    feat(opti): optimized generation of points, significant at larger scale of points

--- script.js
+++ script.js
@@ -5,26 +5,28 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    function generateRandomPoint() {
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
-        return { x, y };
+    function generateRandomPoints(numPoints) {
+        const points = new Float32Array(numPoints * 2);
+
+        for (let i = 0; i < numPoints * 2; i += 2) {
+            points[i] = Math.random() * canvas.width;
+            points[i + 1] = Math.random() * canvas.height;
+        }
+
+        return points;
     }
 
-    function renderPoints(numPoints, pointSize) {
+    function renderPoints(points, pointSize) {
         ctx.fillStyle = '#000';
-        for (let i = 0; i < numPoints; i++) {
-            const point = generateRandomPoint();
-            ctx.fillRect(point.x, point.y, pointSize, pointSize);
+        for (let i = 0; i < points.length; i += 2) {
+            ctx.fillRect(points[i], points[i + 1], pointSize, pointSize);
         }
     }
 
     const numPoints = 100000/10;
-    const pointSize = 2;
-    renderPoints(numPoints, pointSize);
+    const pointSize = 2; //thickness of the points, adjust for visibility
+    const points = generateRandomPoints(numPoints);
+    renderPoints(points, pointSize);
 });
 
-// optimisation of generation
-// divide the canvas into 4 quadrants and generate points in each quadrant
-// instead of fillRect aka drawing one by one, draw all at once? (same issue as writing char by char in c instead of the whole string at once)
-
+// 0,01 second slower than the previous one but on larger scale is significantly faster
