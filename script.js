f0a2c3f14d2acf371c3aee7d107371a98b8a6314
Laura <laura.mastio@epitech.eu>
2023-12-07 14:47:45 +0100

    feat(closest point): brute forcing to find closest point

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    var mouseX = 0, mouseY = 0;
+
+    const numPoints = 1000000/10;
+    const pointSize = 2; //thickness of the points, adjust for visibility
+    const points = generateRandomPoints(numPoints);
+
     function generateRandomPoints(numPoints) {
         const points = new Float32Array(numPoints * 2);
 
@@ -16,17 +22,47 @@ document.addEventListener('DOMContentLoaded', function () {
         return points;
     }
 
+    function findClosestPoint(mouseX, mouseY, points) {
+        console.log("mouseX: ", mouseX)
+        console.log("mouseY: ", mouseY)
+        let closestPoint = null;
+        let minDistance = Number.MAX_VALUE;
+        console.log("minDistance: ", minDistance)
+
+        for (let i = 0; i < points.length; i += 2) {
+            const x = points[i];
+            const y = points[i + 1];
+            const distance = Math.sqrt((mouseX - x) ** 2 + (mouseY - y) ** 2);
+            console.log("distance: ", distance)
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPoint = { x, y };
+            }
+        }
+        return closestPoint;
+    }
+
     function renderPoints(points, pointSize) {
         ctx.fillStyle = '#000';
+        const closestPoint = findClosestPoint(mouseX, mouseY, points);
         for (let i = 0; i < points.length; i += 2) {
+            if (closestPoint == null) {
+                console.log('no closest point found')
+            }
+            else if (points[i] === closestPoint.x && points[i + 1] === closestPoint.y) {
+                ctx.fillStyle = '#f00';
+            } else {
+                ctx.fillStyle = '#000';
+            }
             ctx.fillRect(points[i], points[i + 1], pointSize, pointSize);
         }
     }
 
-    const numPoints = 100000/10;
-    const pointSize = 2; //thickness of the points, adjust for visibility
-    const points = generateRandomPoints(numPoints);
-    renderPoints(points, pointSize);
-});
+    canvas.addEventListener('mousemove', function(event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        renderPoints(points, pointSize);
+    });
 
-// 0,01 second slower than the previous one but on larger scale is significantly faster
+});
